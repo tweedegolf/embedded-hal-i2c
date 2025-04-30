ca8c9ee30d931e00bda70c17baa7adc70d970fe3
Tamme Dittrich <tamme@tweedegolf.com>
2025-04-30 15:09:58 +0200

    Cleanup interface, allowing for both seven and ten bit addresses and removinf done()

--- simulator/src/lib.rs
+++ simulator/src/lib.rs
@@ -449,13 +449,16 @@ enum TargetState {
 mod tests {
     use super::*;
 
+    const A7: u8 = 0x42;
+    const ADDR: AnyAddress = AnyAddress::Seven(A7);
+
     #[tokio::test]
     async fn write_read() {
-        let (mut c, mut t) = simulator(0x42_u8);
+        let (mut c, mut t) = simulator(ADDR);
 
         let control = async move {
             let mut response = [0; 8];
-            c.write_read(0x42, &[1, 2, 3, 4], &mut response)
+            c.write_read(A7, &[1, 2, 3, 4], &mut response)
                 .await
                 .unwrap();
 
@@ -468,7 +471,7 @@ mod tests {
                 panic!()
             };
 
-            assert_eq!(address, 0x42);
+            assert_eq!(address, ADDR);
             let mut buffer = [0; 4];
             let written = handler.handle_complete(&mut buffer).await.unwrap();
             assert_eq!(written, 4);
@@ -478,7 +481,7 @@ mod tests {
             else {
                 panic!()
             };
-            assert_eq!(address, 0x42);
+            assert_eq!(address, ADDR);
             let buffer = [1, 2, 3, 4, 5, 6, 7, 8];
             handler.handle_complete(&buffer, 0xFF).await.unwrap();
         };
@@ -488,22 +491,22 @@ mod tests {
 
     #[tokio::test]
     async fn nacking_everything() {
-        let (mut c, mut t) = simulator(0x42_u8);
+        let (mut c, mut t) = simulator(ADDR);
 
         let control = async move {
-            let result = c.read(0x42, &mut []).await.unwrap_err();
+            let result = c.read(A7, &mut []).await.unwrap_err();
             assert_eq!(
                 result,
                 ErrorKind::NoAcknowledge(NoAcknowledgeSource::Address)
             );
 
-            let result = c.write(0x42, &[]).await.unwrap_err();
+            let result = c.write(A7, &[]).await.unwrap_err();
             assert_eq!(
                 result,
                 ErrorKind::NoAcknowledge(NoAcknowledgeSource::Address)
             );
 
-            let result = c.write(0x42, &[1, 2, 3]).await.unwrap_err();
+            let result = c.write(A7, &[1, 2, 3]).await.unwrap_err();
             assert_eq!(result, ErrorKind::NoAcknowledge(NoAcknowledgeSource::Data));
         };
 
@@ -512,21 +515,21 @@ mod tests {
             else {
                 panic!()
             };
-            assert_eq!(address, 0x42);
-            handler.done().await;
+            assert_eq!(address, ADDR);
+            drop(handler);
 
             let Transaction::WriteTransaction { address, handler } = t.listen().await.unwrap()
             else {
                 panic!()
             };
-            assert_eq!(address, 0x42);
-            handler.done().await;
+            assert_eq!(address, ADDR);
+            drop(handler);
 
             let Transaction::WriteTransaction { address, handler } = t.listen().await.unwrap()
             else {
                 panic!()
             };
-            assert_eq!(address, 0x42);
+            assert_eq!(address, ADDR);
             handler.handle_complete(&mut [0]).await.unwrap();
 
             // Only drop once we are done
@@ -538,7 +541,7 @@ mod tests {
 
     #[tokio::test]
     async fn long_transation() {
-        let (mut c, mut t) = simulator(0x42_u8);
+        let (mut c, mut t) = simulator(ADDR);
 
         let control = async move {
             let mut a = [0];
@@ -552,7 +555,7 @@ mod tests {
                 Operation::Write(&[6]),
             ];
 
-            c.transaction(0x42, &mut transactions).await.unwrap();
+            c.transaction(A7, &mut transactions).await.unwrap();
 
             assert_eq!(a, [3]);
             assert_eq!(b, [4]);
@@ -564,7 +567,7 @@ mod tests {
                 else {
                     panic!()
                 };
-                assert_eq!(address, 0x42);
+                assert_eq!(address, ADDR);
                 let mut buf = [0];
                 let len = handler.handle_complete(&mut buf).await.unwrap();
                 assert_eq!(&buf[..len], [expect]);
@@ -575,7 +578,7 @@ mod tests {
                 else {
                     panic!()
                 };
-                assert_eq!(address, 0x42);
+                assert_eq!(address, ADDR);
                 let ReadResult::Finished(len) = handler.handle_part(&[expect, 0]).await.unwrap()
                 else {
                     panic!()
@@ -588,7 +591,7 @@ mod tests {
                 else {
                     panic!()
                 };
-                assert_eq!(address, 0x42);
+                assert_eq!(address, ADDR);
                 let mut buf = [0];
                 let len = handler.handle_complete(&mut buf).await.unwrap();
                 assert_eq!(&buf[..len], [expect]);
